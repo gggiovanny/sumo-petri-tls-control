b140d7b72b765e57137f9abb1da21c12530a9d4d
gggiovanny <giovanny.gonzalez.19@gmail.com>
2020-03-10 14:37:30 -0600

    Bindeadas acciones con lamdas

--- demo_petri.py
+++ demo_petri.py
@@ -5,26 +5,6 @@ import petri
 def hacerAlgo():
     print("haciendo algo...")
 
-def cambiarFase0():
-    traci.trafficlight.setPhase("semaforo_principal", 0)
-def cambiarFase1():
-    traci.trafficlight.setPhase("semaforo_principal", 1)
-def cambiarFase2():
-    traci.trafficlight.setPhase("semaforo_principal", 2)
-def cambiarFase3():
-    traci.trafficlight.setPhase("semaforo_principal", 3)
-
-def cambiarFaseManual0():
-    traci.trafficlight.setRedYellowGreenState("semaforo_principal", "GrGr")
-def cambiarFaseManual1():
-    traci.trafficlight.setRedYellowGreenState("semaforo_principal", "yryr")
-def cambiarFaseManual2():
-    traci.trafficlight.setRedYellowGreenState("semaforo_principal", "rGrG")
-def cambiarFaseManual3():
-    traci.trafficlight.setRedYellowGreenState("semaforo_principal", "ryry")
-
-
-
 def generatePetriNet():
     # Generando una lista de objetos Places() (Lugares) en una lista llamada 'places'
     places = petri.generatePlaces(range(4))
@@ -45,15 +25,15 @@ def generatePetriNet():
     transition[3].wait_time = 35
     transition[0].wait_time = 6
     
-    # transition[0].action = cambiarFase0
-    # transition[1].action = cambiarFase1
-    # transition[2].action = cambiarFase2
-    # transition[3].action = cambiarFase3
+    transition[0].action = lambda:traci.trafficlight.setRedYellowGreenState("semaforo_principal", "GrGr")
+    transition[1].action = lambda:traci.trafficlight.setRedYellowGreenState("semaforo_principal", "yryr")
+    transition[2].action = lambda:traci.trafficlight.setRedYellowGreenState("semaforo_principal", "rGrG")
+    transition[3].action = lambda:traci.trafficlight.setRedYellowGreenState("semaforo_principal", "ryry")
     
-    transition[0].action = cambiarFaseManual0
-    transition[1].action = cambiarFaseManual1
-    transition[2].action = cambiarFaseManual2
-    transition[3].action = cambiarFaseManual3
+    # transition[0].action = lambda:traci.trafficlight.setPhase("semaforo_principal", 0)
+    # transition[1].action = lambda:traci.trafficlight.setPhase("semaforo_principal", 1)
+    # transition[2].action = lambda:traci.trafficlight.setPhase("semaforo_principal", 2)
+    # transition[3].action = lambda:traci.trafficlight.setPhase("semaforo_principal", 3)
     
     initial_state = [1,0,0,0]
     return petri.Network(places, transition, initial_state, 6)
@@ -77,12 +57,9 @@ def run(net):
 if __name__ == "__main__":
     #? genera el archivo de configuracion que controla las rutas, tipos y flujo de los vehiculos
     config.generar_archivo_vehiculos()
+    #? Generando la red de petri
+    net = generatePetriNet()
     #? este es el modo normal de usar traci. sumo es iniciado como un subproceso y entonces el script de python se conecta y ejecuta
     traci.start(['sumo-gui', "-c", config.sumo_data_path+'demo.sumocfg'])
-    
-    net = generatePetriNet()
-    net.printPreMatrix()
-    net.printPosMatrix()
-    
     #? ejecutando la funcion que controla a la simulacion
     run(net)
